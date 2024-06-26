83bcc8e22556968736dec0ad515aa0c1954ab2d6
anaijajo <anaijajohnson@outlook.com>
2024-06-26 19:45:54 -0400

    Finished solution to hello world exercise. Just wrote first version of repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(repeatWord, repeatNumber) {
+    let fullWord = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        fullWord.concat(repeatWord);
+    }
+    return fullWord;
 };
 
 // Do not edit below this line
