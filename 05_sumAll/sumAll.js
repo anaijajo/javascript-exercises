6de42e3865090708555668410247eda63eb579af
anaijajo <anaijajohnson@outlook.com>
2024-07-09 14:13:25 -0400

    added first solution. first test passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let finalSum = 0;
+    for (let num = firstNum; num < lastNum + 1; num++) {
+        finalSum += num;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
