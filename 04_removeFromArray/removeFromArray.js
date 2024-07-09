ba05fbe96945ac757bccfb87da6e4df0daf1b452
anaijajo <anaijajohnson@outlook.com>
2024-07-09 13:58:39 -0400

    removed .skip on all tests. wrote function using .from method and .filter method after failing previous loop method.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(wordArray, ...removedWords) {
+//     for (let i = 0; i < wordArray.length; i++) {
+//         let index;
+//         if (removedWords) {
+//             index = wordArray.indexOf(removedWords);
+//             wordArray.splice(index);
+//         }
+//      }
+//     return [wordArray];
+//     };
+// Options: calculate position within an array and remove  using splice.
+
+const removeFromArray = function(words, ...remove) {
+    return Array.from(words).filter(word => !remove.includes(word));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
