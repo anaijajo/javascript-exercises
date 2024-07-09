fcc1bbd8d81c0534dd6113736ff05ee86a9e8979
anaijajo <anaijajohnson@outlook.com>
2024-07-09 17:08:40 -0400

    Used Number() method to convert returned solutions into numbers. All tests passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(temp) {
   let formula = (temp - 32) * (5/9);
-  return formula.toFixed(1);
+  return Number(formula.toFixed(1));
 };
 
 const convertToFahrenheit = function(temp) {
   let formula = (temp * (9/5) + 32);
-  return formula.toFixed(1);
+  return Number(formula.toFixed(1));
 };
 
 // Do not edit below this line
