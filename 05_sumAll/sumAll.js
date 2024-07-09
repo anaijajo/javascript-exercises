b7963f104d5447cc0a66ec666279cee9e050d138
anaijajo <anaijajohnson@outlook.com>
2024-07-09 14:31:50 -0400

    solved larger number first issue using if else statement to write a seperate loop. all tests passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,16 @@
 const sumAll = function(firstNum, lastNum) {
     if (typeof firstNum !== "number" || firstNum < 0 || typeof lastNum !== "number" || lastNum < 0) return "ERROR";
     let finalSum = 0;
-    for (let num = firstNum; num < lastNum + 1; num++) {
-        finalSum += num;
+    if (firstNum < lastNum) {
+        for (let num = firstNum; num < lastNum + 1; num++) {
+            finalSum += num;
+        }
+    } else {
+        for (let num = lastNum; num < firstNum + 1; num++) {
+            finalSum += num;
+        }
     }
+    
     return finalSum;
 };
 
