08088d011f781a3da3142f3fd04a57458cfa0469
anaijajo <anaijajohnson@outlook.com>
2024-07-09 18:41:05 -0400

    unlocked factorial function tests. passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,8 +26,13 @@ const power = function(num1, num2) {
 	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
